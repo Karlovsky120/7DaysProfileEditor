160724fa43a8b9b0f86c70e9db57ad04eb4e6f2b
Karlovsky120 <karlovsky120@gmail.com>
2016-04-10 19:34:31 +0200

    Added some basic statistic data with locations
    Added score, deaths, kills, player location and rotation, player home
    location and dropped backpack location.

---
+++ source/0.14.5.0/Log.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenDaysProfileEditor
+{
+    class Log
+    {
+        public static void writeError(Exception e)
+        {
+            StreamWriter writer = new StreamWriter("ProfileEditorCrash.log", true);
+
+            writer.WriteLine("Time Stamp: " + DateTime.Now.ToString());
+            writer.WriteLine("Message: " + e.Message);
+            writer.WriteLine("Source: " + e.Source);           
+            writer.WriteLine("Inner Exception: " + e.InnerException);                      
+            writer.WriteLine("Stack Trace: " + e.StackTrace);
+            writer.WriteLine("Target Site: " + e.TargetSite);
+            writer.WriteLine();
+            writer.WriteLine();
+            writer.WriteLine();
+        }
+
+    }
+}
