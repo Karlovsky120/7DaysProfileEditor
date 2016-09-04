77ead31151bb8fbc5275d59d2cf1547432ede0d3
Karlovsky120 <karlovsky120@gmail.com>
2016-09-04 06:14:05 +0200

    Update 0.14.7.0
    You can now edit your stats. Note that I don't know what exactly all
    those numbers mean, so take care with changing them. Also, the save file
    contains another entry for food and water stats, so this particular
    stats may not affect food and water.
    
    Also fixed minor bug regarding saving and closing files when none are
    opened.
    
    7DaysSaveManipulator.dll was not changed. This should also work with
    Alphas 14.5 and 14.6.

---
+++ source/0.14.7.0/LabeledBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SevenDaysProfileEditor.GUI
+{
+    class LabeledBox : TableLayoutPanel
+    {
+        public LabeledBox(string name, Control textBox)
+        {
+            AutoSize = true;
+
+            Label label = new Label();
+            label.Text = name + ":";
+            label.Width = 110;
+            label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+
+            Controls.Add(label, 0, 0);
+            Controls.Add(textBox, 1, 0);
+        }
+    }
+}
---
+++ source/0.14.7.0/SlotStat.cs
@@ -0,0 +1,68 @@
+﻿using SevenDaysProfileEditor.GUI;
+using SevenDaysSaveManipulator.GameData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SevenDaysProfileEditor.Stats
+{
+    class SlotStat : TableLayoutPanel
+    {
+        private Stat stat;
+
+        public void generateSlot(Stat stat, string statName)
+        {
+            Width = 250;
+            Height = 250;
+            CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+
+            Label name = new Label();
+            name.Text = statName;
+            name.AutoSize = true;
+
+            Controls.Add(name);
+
+            TextBoxFloat valueBox = new TextBoxFloat(stat.value, float.MinValue, float.MaxValue);
+            LabeledBox labeledValueBox = new LabeledBox("Value", valueBox);
+
+            Controls.Add(labeledValueBox);
+
+            TextBoxFloat originalValueBox = new TextBoxFloat(stat.originalValue, float.MinValue, float.MaxValue);
+            LabeledBox labeledOriginalValueBox = new LabeledBox("Original Value", originalValueBox);
+
+            Controls.Add(labeledOriginalValueBox);
+
+            TextBoxFloat valueModifierBox = new TextBoxFloat(stat.valueModifier, float.MinValue, float.MaxValue);
+            LabeledBox labeledValueModifierBox = new LabeledBox("Value Modifier", valueModifierBox);
+
+            Controls.Add(labeledValueModifierBox);
+
+            TextBoxFloat baseMaxBox = new TextBoxFloat(stat.baseMax, float.MinValue, float.MaxValue);
+            LabeledBox labeledBaseMaxBox = new LabeledBox("Base Max", baseMaxBox);
+
+            Controls.Add(labeledBaseMaxBox);
+
+            TextBoxFloat originalMaxBox = new TextBoxFloat(stat.originalMax, float.MinValue, float.MaxValue);
+            LabeledBox labeledOriginalMaxBox = new LabeledBox("Original Max", originalMaxBox);
+
+            Controls.Add(labeledValueBox);
+
+            TextBoxFloat maxModifierBox = new TextBoxFloat(stat.maxModifier, float.MinValue, float.MaxValue);
+            LabeledBox labeledMaxModifierBox = new LabeledBox("Max Modifier", maxModifierBox);
+
+            Controls.Add(labeledMaxModifierBox);
+        }
+
+        public SlotStat(Stat stat, string statName) : base()
+        {
+            Dock = DockStyle.Fill;
+
+            this.stat = stat;
+
+            generateSlot(stat, statName);
+        }
+    }
+}
