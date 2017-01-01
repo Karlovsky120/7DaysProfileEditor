da9d0d79d74c573639ef1659a30ca1ee5c566809
Karlovsky120 <karlovsky120@gmail.com>
2017-01-01 20:27:50 +0100

    Fixed atachment bug
    Fixed bug where attachments wouldn't properly load into the program
    (wapons with attachments would look as though they had none (air).
    Fixed bug where saving a file with a weapon with attachment enabled
    would fail.

---
+++ source/1.15.1.1/GUI/MainWindow.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SevenDaysProfileEditor.GUI {
+
+    /// <summary>
+    /// Main window of the program
+    /// </summary>
+    internal class MainWindow : Form {
+        public Label focusDummy;
+        public MainMenuStrip mainMenu;
+        public BottomStatusBar statusBar;
+        public PlayerTabControl tabs;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public MainWindow() {
+            Size = new Size(1000, 850);
+
+            Text = "7 Days Profile Editor";
+            StartPosition = FormStartPosition.CenterScreen;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+
+            tabs = new PlayerTabControl(this);
+            statusBar = new BottomStatusBar();
+            mainMenu = new MainMenuStrip(this, tabs, statusBar);
+
+            focusDummy = new Label();
+            focusDummy.Size = new Size(0, 0);
+            Controls.Add(focusDummy);
+
+            Controls.Add(tabs);
+            Controls.Add(mainMenu);
+            Controls.Add(statusBar);
+        }
+    }
+}
---
+++ source/1.15.1.1/Program.cs
@@ -0,0 +1,84 @@
+﻿using SevenDaysProfileEditor.Data;
+using SevenDaysProfileEditor.GUI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SevenDaysProfileEditor {
+
+    internal static class Program {
+
+        /// <summary>
+        /// Initializes the static data of the program
+        /// </summary>
+        private static void Initialize() {
+            try {
+                AssetInfo.GenerateAssetInfoList();
+                IconData.itemIconDictionary = new Dictionary<string, byte[]>();
+                IconData.uiIconDictionary = new Dictionary<string, UIIconData>();
+                IconData.PopulateIconDictionaries();
+            }
+            catch (Exception e) {
+                Log.WriteException(e);
+                MessageBox.Show("No icons will be loaded. Failed to load asset files." + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            try {
+                XmlData.GetBlocks();
+                XmlData.GetItems();
+                XmlData.ArrangeItemList();
+                XmlData.GetSkills();
+                AssetInfo.ClearAssetInfoList();
+            }
+            catch (Exception e) {
+                Log.WriteException(e);
+                MessageBox.Show(string.Format("Failed to load XML files!\n{0}\nProgram will now terminate!", e.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
+        }
+
+        [STAThread]
+        private static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            MainWindow window = new MainWindow();
+            Config.Load();
+
+            string gameRoot = Config.GetSetting("gameRoot");
+
+            // If no game root is specified in the config, we check if we are inside the game root. If not, we ask the user.
+            if (gameRoot == null) {
+                if (File.Exists(Environment.CurrentDirectory + "7DaysToDie.exe")) {
+                    Initialize();
+                    window.Show();
+                }
+                else {
+                    OpenFileDialog gameRootDialog = new OpenFileDialog() {
+                        Title = "Tool needs to find the 7DaysToDie.exe!",
+                        Filter = "7DaysToDie.exe|7DaysToDie.exe"
+                    };
+
+                    gameRootDialog.FileOk += (sender1, e1) => {
+                        gameRoot = gameRootDialog.FileName.Substring(0, gameRootDialog.FileName.LastIndexOf('\\'));
+                        Config.SetSetting("gameRoot", gameRoot);
+                        Initialize();
+                        window.Show();
+                    };
+
+                    if (gameRootDialog.ShowDialog() != DialogResult.OK) {
+                        Application.Exit();
+                        return;
+                    }
+                }
+            }
+            else {
+                Initialize();
+                window.Show();
+            }
+
+            Application.Run(window);
+        }
+    }
+}
